d7d674793ca0a3779e088307c468b8447cb8e361
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-01-27 20:56:56 +0000

    add notebook title

--- 07_ar_model_prices.py
+++ 07_ar_model_prices.py
@@ -1,18 +1,19 @@
 import marimo
 
 __generated_with = "0.15.2"
-app = marimo.App(width="full")
+app = marimo.App(width="full", app_title="07. The AR Model - Prices")
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(r"""# 07. The AR Model - Prices""")
     return
 
 
 @app.cell
 def _():
     import marimo as mo
-    return
+    return (mo,)
 
 
 if __name__ == "__main__":
