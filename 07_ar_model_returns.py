0fd2586a83021f50033361c4414b07e92e103073
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-01-29 15:05:07 +0000

    add app title

---
+++ 07_ar_model_returns.py
@@ -0,0 +1,14 @@
+import marimo
+
+__generated_with = "0.19.6"
+app = marimo.App(width="full", app_title="07. The AR Model - Returns")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+if __name__ == "__main__":
+    app.run()
