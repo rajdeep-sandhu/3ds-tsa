af8f57559739b4c6e5f9948af04a131f3b911882
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-02 21:43:06 +0000

    add title

---
+++ 08_ma_model.py
@@ -0,0 +1,16 @@
+import marimo
+
+__generated_with = "0.19.6"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # 08. The MA Model
+    """)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
